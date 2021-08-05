62b73faf8c68b8c34301217904031cb1ef8df755
Uldis Baurovskis <8036190+hansagames@users.noreply.github.com>
2021-08-05 23:32:26 +0300

    Fix type issues (#16)

--- src/coconut/feathersui/Implicit.hx
+++ src/coconut/feathersui/Implicit.hx
@@ -1,12 +1,11 @@
 package coconut.feathersui;
 
-import coconut.diffing.Implicit;
-import coconut.diffing.VNode.VNodeData.VWidget;
+import coconut.diffing.internal.VWidget;
 import coconut.feathersui.RenderResult;
 import feathers.core.ValidatingSprite;
 
 class Implicit extends Implicit<ValidatingSprite, RenderResult> {
-	static final TYPE = Implicit.type();
+	static final TYPE = mplicit.type();
 
 	static public function fromHxx(attr):RenderResult {
 		return VWidget(TYPE, null, null, attr);
