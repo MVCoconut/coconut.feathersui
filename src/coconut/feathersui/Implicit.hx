26f897ab96f1b2ad2cf0a51a8ba3e3e3e6c27d66
Uldis Baurovskis <8036190+hansagames@users.noreply.github.com>
2021-08-06 21:56:57 +0300

    Fix implicit typings (#17)

--- src/coconut/feathersui/Implicit.hx
+++ src/coconut/feathersui/Implicit.hx
@@ -1,13 +1,3 @@
 package coconut.feathersui;
 
-import coconut.diffing.internal.VWidget;
-import coconut.feathersui.RenderResult;
-import feathers.core.ValidatingSprite;
-
-class Implicit extends Implicit<ValidatingSprite, RenderResult> {
-	static final TYPE = mplicit.type();
-
-	static public function fromHxx(attr):RenderResult {
-		return VWidget(TYPE, null, null, attr);
-	}
-}
+class Implicit extends coconut.diffing.Implicit<feathers.core.ValidatingSprite, coconut.feathersui.RenderResult> {}
